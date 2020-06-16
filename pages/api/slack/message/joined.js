96e272f6d375f67661f552bc92a2e7e41bf153bb
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-16 11:30:28 -0400

    d

--- pages/api/slack/message/joined.js
+++ pages/api/slack/message/joined.js
@@ -6,7 +6,7 @@ import { postEphemeral, timeout } from "../../../../lib/api-utils"
 export default async (req, res) => {
   const { user, channel } = req.body.event
 
-  await timeout(500)
+  await timeout(1500)
   await postEphemeral(channel, `Welcome to the Summer Scrapbook, <@${user}>!
   To get started, post a photo or video of a project you're working on—it can be anything!
   Your update will be added to your personal scrapbook, which I'll share with you after your
