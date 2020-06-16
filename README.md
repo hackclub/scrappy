a577dedae5efbedbd2aa737cbf7d36ac1c2bea78
Max Wofford <max@maxwofford.com>
2020-06-16 01:00:45 -0400

    Attempt to fix message challenge response

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # [Scrappy bot](https://scrapbook.hackclub.com/)
+
+Environmental variables:
+
+- `VC_SCRAPBOOK_TOKEN`
+- `CHANNEL`
+- `MUX_SIGNING`
+- `MUX_SIGNING_SECRET`
+- `MUX_TOKEN_SECRET`
+- `MUX_TOKEN_ID`
+- `SLACK_USER_TOKEN`
+- `AIRTABLE_API_KEY`
+- `SLACK_BOT_TOKEN`
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const next = require('next')
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express();
-  server.use(bodyParser.urlencoded({ extended: true }))
-  server.use(bodyParser.json())
+  const server = express()
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-  server.listen(process.env.PORT || 3000, err => {
+  server.all('*', (req, res) => handle(req, res))
+
+  const port = process.env.PORT || 3000
+  server.listen(port, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:' + port)
   })
 })
