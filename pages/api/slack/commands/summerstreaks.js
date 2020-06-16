26f11ad2631fdc4cd5aa2fe2a07c2ec92056931e
Max Wofford <max@maxwofford.com>
2020-06-16 15:42:13 -0400

    Add error handling :shrug:

--- pages/api/slack/commands/summerstreaks.js
+++ pages/api/slack/commands/summerstreaks.js
@@ -6,18 +6,22 @@ import {
 } from '../../../../lib/api-utils'
 
 export default async (req, res) => {
-  console.log("Running summerstreaks.js")
-  const userId = req.body.user_id
-  const record = await getUserRecord(userId)
-  const display = record.fields['Display Streak']
-  await accountsTable.update(record.id, { 'Display Streak': !display })
-  await sendCommandResponse(
-    req.body.response_url,
-    display
-      ? `Your streak count is now invisible.`
-      : `Your streak count is now visible!`
-  )
-  res.status(200).end()
-  const streakCount = record.fields['Streak Count']
-  await displayStreaks(userId, streakCount)
+  try {
+    console.log('Running summerstreaks.js')
+    const userId = req.body.user_id
+    const record = await getUserRecord(userId)
+    const display = record.fields['Display Streak']
+    await accountsTable.update(record.id, { 'Display Streak': !display })
+    await sendCommandResponse(
+      req.body.response_url,
+      display
+        ? `Your streak count is now invisible.`
+        : `Your streak count is now visible!`
+    )
+    res.status(200).end()
+    const streakCount = record.fields['Streak Count']
+    await displayStreaks(userId, streakCount)
+  } catch (e) {
+    console.error(e)
+  }
 }
