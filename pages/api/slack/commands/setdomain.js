eb82a284f2e4fdf8c309afd6101e4f2818a02523
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-16 12:55:55 -0400

    Fix /setdomain

--- pages/api/slack/commands/setdomain.js
+++ pages/api/slack/commands/setdomain.js
@@ -22,41 +22,47 @@ export default async (req, res) => {
     })
     const domainCount = updates.length
 
-    const vercelFetch = await fetch(
-      `https://api.vercel.com/v1/projects/QmbACrEv2xvaVA3J5GWKzfQ5tYSiHTVX2DqTYfcAxRzvHj/alias?teamId=team_gUyibHqOWrQfv3PDfEUpB45J`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.VC_SCRAPBOOK_TOKEN}`
-        },
-        body: JSON.stringify({
-          domain: command.text
-        })
-      }
-    )
-      .then(r => r.json())
-      .catch(err => {
-        console.log(`Error while setting custom domain ${command.text}: ${err}`)
-        if (domainCount > 50) {
-          sendCommandResponse(
-            command.response_url,
-            `Couldn't set your domain. Only 50 custom domains can be added to a project, and 50 people have already added their custom domains. :/`
-          )
+    if (domainCount > 50) {
+      sendCommandResponse(
+        command.response_url,
+        `Couldn't set your domain. Only 50 custom domains can be added to a project, and 50 people have already added their custom domains. :/`
+      )
+    }
+    else {
+      const vercelFetch = await fetch(
+        `https://api.vercel.com/v1/projects/QmbACrEv2xvaVA3J5GWKzfQ5tYSiHTVX2DqTYfcAxRzvHj/alias?teamId=team_gUyibHqOWrQfv3PDfEUpB45J`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.VC_SCRAPBOOK_TOKEN}`
+          },
+          body: JSON.stringify({
+            domain: command.text
+          })
         }
+      )
+        .then(r => r.json())
+        .catch(err => {
+          console.log(`Error while setting custom domain ${command.text}: ${err}`)
+        })
+      console.log(vercelFetch)
+      if (vercelFetch.error) {
         sendCommandResponse(
           command.response_url,
           `Couldn't set your domain. You can't add a domain if it's already set to another Vercel project. Try again with a different domain.`
         )
-      })
-    console.log(vercelFetch)
-    sendCommandResponse(
-      command.response_url,
-      `Custom domain \`${command.text}\` set!\n\n
-    *Your next steps*: create a CNAME record in your DNS provider for your domain and point it to \`cname.vercel-dns.com\`. \n\n
-    You're one of 50 people who can add a custom domain during the Summer of Making. There are *${domainCount}* domains spots left.
-    `
-    )
+      }
+      else {
+        sendCommandResponse(
+          command.response_url,
+          `Custom domain \`${command.text}\` set!\n\n
+        *Your next steps*: create a CNAME record in your DNS provider for your domain and point it to \`cname.vercel-dns.com\`. \n\n
+        You're one of 50 people who can add a custom domain during the Summer of Making. There are *${50 - domainCount}* domains spots left.
+        `
+        )
+      }
+    }
   }
 
   res.status(200).end()
