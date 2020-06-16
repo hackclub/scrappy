988d186491fe36b9830145e29cd3271c30d4c14e
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-16 11:27:43 -0400

    2 things
    * Fix record not creating if user has no profile fields
    * Tiemout 500ms when user joins private channel so that ephemeral message sends

--- pages/api/slack/message/joined.js
+++ pages/api/slack/message/joined.js
@@ -1,11 +1,12 @@
 /*
 This posts an introductory message to the #scrapbook channel when someone shows up
 */
-import { postEphemeral } from "../../../../lib/api-utils"
+import { postEphemeral, timeout } from "../../../../lib/api-utils"
 
 export default async (req, res) => {
   const { user, channel } = req.body.event
 
+  await timeout(500)
   await postEphemeral(channel, `Welcome to the Summer Scrapbook, <@${user}>!
   To get started, post a photo or video of a project you're working on—it can be anything!
   Your update will be added to your personal scrapbook, which I'll share with you after your
