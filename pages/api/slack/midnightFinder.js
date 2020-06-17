154487bf35e547d21b2a668eb4f37fbdf6227084
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-16 22:33:20 -0400

    remove test

--- pages/api/slack/midnightFinder.js
+++ pages/api/slack/midnightFinder.js
@@ -13,42 +13,42 @@ export default async (req, res) => {
     const timeString = new Date(localTime).toUTCString()
 
     //console.log(username, timeString)
-    //if (timeString.split(' ')[4].includes('00:')) {
-    const latestUpdate = await updatesTable.read({
-      maxRecords: 1,
-      sort: [{ field: 'Post Time', direction: 'desc' }],
-      filterByFormula: `FIND('${username}', {ID}) > 0`
-    })
-    const createdTime = Date.parse(latestUpdate[0]?.fields['Post Time'])
-    if (Date.now() - createdTime > 86400000) {
-      console.log(`It's midnight for ${username} and it's been 48 hours since they last posted. Resetting their streak...`)
-      accountsTable.update(user.id, {
-        'Streak Count': 0
+    if (timeString.split(' ')[4].includes('00:')) {
+      const latestUpdate = await updatesTable.read({
+        maxRecords: 1,
+        sort: [{ field: 'Post Time', direction: 'desc' }],
+        filterByFormula: `FIND('${username}', {ID}) > 0`
       })
-      await fetch('https://slack.com/api/users.profile.set', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.SLACK_USER_TOKEN}`
-        },
-        body: JSON.stringify({
-          user: userId,
-          profile: { status_text: '', status_emoji: `` }
+      const createdTime = Date.parse(latestUpdate[0]?.fields['Post Time'])
+      if (Date.now() - createdTime > 86400000) {
+        console.log(`It's midnight for ${username} and it's been 48 hours since they last posted. Resetting their streak...`)
+        accountsTable.update(user.id, {
+          'Streak Count': 0
         })
-      })
-      fetch('https://slack.com/api/chat.postMessage', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
-        },
-        body: JSON.stringify({
-          channel: userId,
-          text: `It's been 48 hours since you last posted a Scrapbook update, so I've reset your streak. No worries, though—post something else to start another streak! And the rest of your updates are still available at https://scrapbook.hackclub.com/${username} :)`
+        await fetch('https://slack.com/api/users.profile.set', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.SLACK_USER_TOKEN}`
+          },
+          body: JSON.stringify({
+            user: userId,
+            profile: { status_text: '', status_emoji: `` }
+          })
         })
-      })
-      //console.log(username, latestUpdate[0].fields['Post Time'], new Date(Date.now()).toUTCString(), Date.now() - createdTime)
+        fetch('https://slack.com/api/chat.postMessage', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+          },
+          body: JSON.stringify({
+            channel: userId,
+            text: `It's been 48 hours since you last posted a Scrapbook update, so I've reset your streak. No worries, though—post something else to start another streak! And the rest of your updates are still available at https://scrapbook.hackclub.com/${username} :)`
+          })
+        })
+        //console.log(username, latestUpdate[0].fields['Post Time'], new Date(Date.now()).toUTCString(), Date.now() - createdTime)
+      }
     }
-    //}
   })
 }
