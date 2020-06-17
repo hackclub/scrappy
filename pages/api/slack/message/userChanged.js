18b9938f3dbf9d8bf6e4480095bdaf3647593aef
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-17 16:49:52 -0400

    debhdebug

--- pages/api/slack/message/userChanged.js
+++ pages/api/slack/message/userChanged.js
@@ -8,6 +8,7 @@ export default async (req, res) => {
     const statusEmojiCount = statusEmoji.split('-')[1].split(':')[0]
     console.log('count', statusEmojiCount)
     const userRecord = await getUserRecord(user.id)
+    console.log('user record count', userRecord.fields['Streak Count'])
     if (userRecord.fields['Streak Count'] !== statusEmojiCount) {
       setStatus(user.id, `I tried to cheat in Summer of Making because I'm a clown`, ':clown_face:')
     }
