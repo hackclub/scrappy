72e5a7ba8776de3c5b26957232d5fb4318e3d1c9
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-18 05:50:27 -0400

    Delete messgae not scrap

--- pages/api/slack/message/noFile.js
+++ pages/api/slack/message/noFile.js
@@ -1,10 +1,24 @@
-import { deleteScrap, postEphemeral, t } from "../../../../lib/api-utils"
+import { postEphemeral, t } from "../../../../lib/api-utils"
 
 export default async (req, res) => {
   const { channel, ts, user } = req.body.event
 
   await Promise.all([
-    deleteScrap(ts),
+    deleteMessage(channel, ts),
     postEphemeral(channel, t('messages.delete'), user)
   ])
 }
+
+const deleteMessage = async (channel, ts) => (
+  fetch('https://slack.com/api/chat.delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`
+    },
+    body: JSON.stringify({
+      channel,
+      ts
+    })
+  })
+)
