4333e63a61f4f48ae864672bf865d772c79a6413
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-18 10:54:00 -0400

    idk lol

--- pages/api/slack/message/joined.js
+++ pages/api/slack/message/joined.js
@@ -1,11 +1,12 @@
 /*
 This posts an introductory message to the #scrapbook channel when someone shows up
 */
-import { postEphemeral, t } from '../../../../lib/api-utils'
+import { postEphemeral, t, timeout } from '../../../../lib/api-utils'
 
 export default async (req, res) => {
   const { user, channel } = req.body.event
 
+  await timeout(1000)
   postEphemeral(channel, t(channel == process.env.CHANNEL ? 'messages.join.scrapbook' : 'messages.join.css', { user }), user)
 
   return res.json({ ok: true })
