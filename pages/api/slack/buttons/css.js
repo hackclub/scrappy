02da871e1f9fc5bd1558d61e1c54dcdbef7466f8
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-18 05:20:23 -0400

    test

--- pages/api/slack/buttons/css.js
+++ pages/api/slack/buttons/css.js
@@ -7,20 +7,15 @@ export default async (req, res) => {
   const responseUrl = data.response_url
 
   const parentMessage = await fetch(`https://slack.com/api/conversations.history?token=${process.env.SLACK_BOT_TOKEN}&channel=${data.channel.id}&latest=${data.message.thread_ts}&limit=1&inclusive=true`).then(r => r.json())
-  console.log('parentmessage', parentMessage)
   const text = parentMessage.messages[0].text
-  console.log('text', text)
   let url = getUrlFromString(text)
-  console.log(url)
   if (url.includes('gist.github.com')) {
     url = await processGist(url)
-    console.log('processed', url)
   }
-  console.log(url)
 
   const userRecord = await getUserRecord(userId)
   await accountsTable.update(userRecord.id, {
     'CSS URL': url
   })
-  await postEphemeral('C015M6U6JKU', t('messages.css.set', { url, username: userRecord.fields['Username'] }), userId, data.message.thread_ts)
+  await postEphemeral('C015M6U6JKU', t('messages.css.set', { url, username: userRecord.fields['Username'] }), userId)
 }
