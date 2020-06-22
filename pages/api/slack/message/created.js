f0f9673181f00c90c9456df3af8915a85871e4dd
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-22 12:29:36 -0400

    Fix formatting messages
    Attempt 1, not tested

--- pages/api/slack/message/created.js
+++ pages/api/slack/message/created.js
@@ -65,7 +65,7 @@ export default async (req, res) => {
 
   const date = new Date().toLocaleString("en-US", { timeZone: userRecord.fields['Timezone'] })
   const convertedDate = new Date(date).toISOString()
-  const message = replaceEmoji(text)
+  const message = formatText(text)
   console.log(convertedDate)
 
   await updatesTable.create({
