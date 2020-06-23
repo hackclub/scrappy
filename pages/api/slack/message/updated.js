38d50f82132a54c4cf375574fda309975687ef3a
Lachlan Campbell <lachlan@hackclub.com>
2020-06-23 01:02:47 -0400

    Re-format text on message edit

--- pages/api/slack/message/updated.js
+++ pages/api/slack/message/updated.js
@@ -1,13 +1,21 @@
 /*
 This message is called when a poster updates their previous post
 */
-const { react, postEphemeral, updatesTable, getUserRecord, replaceEmoji, fetchProfile, unverifiedRequest } = require("../../../../lib/api-utils")
+const {
+  react,
+  postEphemeral,
+  updatesTable,
+  getUserRecord,
+  formatText,
+  fetchProfile,
+  unverifiedRequest,
+} = require('../../../../lib/api-utils')
 
 export default async (req, res) => {
   if (unverifiedRequest(req)) return res.status(400).send('Unverified Slack request!')
   else res.status(200).end()
 
-  const newMessage = replaceEmoji(req.body.event.message.text)
+  const newMessage = formatText(req.body.event.message.text)
   const prevTs = req.body.event.previous_message.ts
 
   await react('add', req.body.event.channel, prevTs, 'beachball')
@@ -16,12 +24,14 @@ export default async (req, res) => {
     maxRecords: 1,
     filterByFormula: `{Message Timestamp} = '${prevTs}'`
   }))[0]
-  await updatesTable.update(updateRecord.id, {
-    'Text': newMessage
-  })
+  await updatesTable.update(updateRecord.id, { Text: newMessage })
   await Promise.all([
     react('remove', req.body.event.channel, prevTs, 'beachball'),
-    postEphemeral(req.body.event.channel, `Your update has been edited! You should see it update on the website in a few seconds.`, req.body.event.message.user)
+    postEphemeral(
+      req.body.event.channel,
+      `Your post has been edited! You should see it update on the website in a few seconds.`,
+      req.body.event.message.user
+    )
   ])
   const userRecord = await getUserRecord(req.body.event.user)
   fetchProfile(userRecord.fields['Username'])
