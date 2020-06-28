8c3ee38d55beaa7053a7fbe2fa7a19244faba537
Saharsh Yeruva <github@saharsh.tech>
2020-06-28 15:40:17 -0400

    update scrappy user on profile change (#32)
    * update github and website on profile change
    
    * Update pfp on profile change
    
    * Reorganize a bit + small edits
    
    Co-authored-by: Matthew Stanciu <mattbstanciu@gmail.com>

--- pages/api/slack/message/userChanged.js
+++ pages/api/slack/message/userChanged.js
@@ -1,7 +1,12 @@
-import { getUserRecord, setStatus, unverifiedRequest } from '../../../../lib/api-utils'
+import {
+  getUserRecord,
+  setStatus,
+  unverifiedRequest
+} from '../../../../lib/api-utils'
 
 export default async (req, res) => {
-  if (unverifiedRequest(req)) return res.status(400).send('Unverified Slack request!')
+  if (unverifiedRequest(req))
+    return res.status(400).send('Unverified Slack request!')
   else res.status(200).end()
 
   console.log(req.body)
@@ -9,14 +14,57 @@ export default async (req, res) => {
   const user = req.body.event.user
   const statusEmoji = user.profile.status_emoji
 
+  const { user_id, response_url } = req.body
+
+  const userRecord = await getUserRecord(user_id)
+
   if (statusEmoji.includes('som-')) {
     const statusEmojiCount = statusEmoji.split('-')[1].split(':')[0]
     console.log('count', statusEmojiCount)
     const userRecord = await getUserRecord(user.id)
     const streakCount = userRecord.fields['Streak Count']
     console.log('user record count', streakCount)
-    if (((streakCount != statusEmojiCount) && (streakCount <= 7)) || (('7+' != statusEmojiCount) && (streakCount >= 8))) {
-      setStatus(user.id, `I tried to cheat in Summer of Making because I’m a clown`, ':clown_face:')
+    if (
+      (streakCount != statusEmojiCount && streakCount <= 7) ||
+      ('7+' != statusEmojiCount && streakCount >= 8)
+    ) {
+      setStatus(
+        user.id,
+        `I tried to cheat in Summer of Making because I’m a clown`,
+        ':clown_face:'
+      )
     }
+  }
+  
+  // While we're here, check if any of the user's profile fields have been changed & update them in Airtable
+  
+  const info = await fetch(
+    `https://slack.com/api/users.info?token=${process.env.SLACK_BOT_TOKEN}&user=${user_id}`
+  ).then((r) => r.json())
+  const tzOffset = info.user.tz_offset
+  const tz = info.user.tz.replace(`\\`, '')
+  
+  const avatar = user.profile.image_192 // user from the event
+  const github = user.profile.fields['Xf0DMHFDQA']?.value
+  const website = user.profile.fields['Xf5LNGS86L']?.value
+
+  if (github != userRecord.fields['GitHub']) {
+    accountsTable.update(userRecord.id, {
+      GitHub: github
+    })
+  }
+  if (website != userRecord.fields['Website']) {
+    accountsTable.update(userRecord.id, {
+      Website: website
+    })
+  }
+  accountsTable.update(userRecord.id, {
+    Timezone: tz,
+    'Timezone offset': tzOffset
+    Avatar: [
+      {
+        url: avatar
+      }
+    ]
   }
 }
