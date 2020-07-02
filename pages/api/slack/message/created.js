f7825281b7c197539d95420584b478c418ef838b
Matthew Stanciu <mattbstanciu@gmail.com>
2020-07-02 10:00:09 -0400

    dfhagsdf

--- pages/api/slack/message/created.js
+++ pages/api/slack/message/created.js
@@ -72,6 +72,7 @@ export default async (req, res) => {
   const fullSlackMember = userRecord.fields['Full Slack Member?']
   if (!fullSlackMember) {
     const fullMember = await isFullMember(user)
+    console.log('is full member', fullMember)
     if (fullMember) {
       accountsTable.update(userRecord.id, { 'Full Slack Member?': true })
     }
