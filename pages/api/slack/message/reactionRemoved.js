6bea8b12fd4eaf241a6f31b2de1a6eeb94ca6430
Matthew Stanciu <mattbstanciu@gmail.com>
2020-07-08 13:18:16 -0400

    Fix getreactionrecord on removed

--- pages/api/slack/message/reactionRemoved.js
+++ pages/api/slack/message/reactionRemoved.js
@@ -10,8 +10,8 @@ export default async (req, res) => {
     maxRecords: 1,
     filterByFormula: `{Message Timestamp} = '${ts}'`
   }))[0]
-  console.log('reaction_removed: update record', update)
-  const reactionRecord = await getReactionRecord(reaction, update.id)
+  //console.log('reaction_removed: update record', update)
+  const reactionRecord = await getReactionRecord(reaction, update.fields['ID'])
   console.log('reaction_removed: reaction record', reactionRecord)
 
   let usersReacted = reactionRecord.fields['Users Reacted']
