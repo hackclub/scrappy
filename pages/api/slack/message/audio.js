02d80d9f82cd3c6291012bd992d05b722e0a1562
Matthew Stanciu <mattbstanciu@gmail.com>
2020-07-08 15:53:01 -0400

    Don't accept unaccepted files types

--- pages/api/slack/message/audio.js
+++ pages/api/slack/message/audio.js
@@ -7,9 +7,20 @@ export default async (req, res) => {
   const { files = [], channel, ts, user, text, thread_ts } = req.body.event
   if (thread_ts) return res.json({ ok: true })
 
-  const userRecord = await getUserRecord(user)
+  const urlPrivate = files[0].url_private
+  const fileName = urlPrivate.split('/').pop()
 
-  const publicUrl = await getPublicFileUrl(files[0].url_private, channel, user)
+  const acceptedFileTypes = ['mp3', 'wav', 'aiff', 'm4a']
+  const containsAcceptedFileTypes = acceptedFileTypes.some((el) =>
+    fileName.toLowerCase().endsWith(el)
+  )
+  if (!containsAcceptedFileTypes) {
+    reply(channel, ts, t('messages.audio.notaccepted'))
+    return res.status(200).end()
+  }
+
+  const userRecord = await getUserRecord(user)
+  const publicUrl = await getPublicFileUrl(urlPrivate, channel, user)
 
   await Promise.all([
     accountsTable.update(userRecord.id, {
