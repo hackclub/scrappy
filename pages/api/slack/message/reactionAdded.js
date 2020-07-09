bc9538ab417676bd70f5e1a149b3dfac79a839f9
Matthew Stanciu <mattbstanciu@gmail.com>
2020-07-09 12:05:34 -0400

    Attempt 1 bottleneck reactions

--- pages/api/slack/message/reactionAdded.js
+++ pages/api/slack/message/reactionAdded.js
@@ -1,4 +1,9 @@
 import { unverifiedRequest, getEmojiRecord, updatesTable, reactionsTable, getUserRecord, getReactionRecord, updateExists, emojiExists } from "../../../../lib/api-utils"
+import Bottleneck from 'bottleneck'
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1
+})
 
 export default async (req, res) => {
   if (unverifiedRequest(req)) return res.status(400).send('Unverified Slack request!')
@@ -12,49 +17,50 @@ export default async (req, res) => {
     console.log('not including default emoji')
     return
   }
+  limiter.schedule(() => {
+    const emojiRecord = await getEmojiRecord(reaction)
+    const userRecord = await getUserRecord(user)
 
-  const emojiRecord = await getEmojiRecord(reaction)
-  const userRecord = await getUserRecord(user)
+    const ts = item.ts
+    const update = (await updatesTable.read({
+      maxRecords: 1,
+      filterByFormula: `{Message Timestamp} = '${ts}'`
+    }))[0]
+    if (!update) {
+      console.log('reaction was added to a message in a thread. skipping...')
+      return
+    }
 
-  const ts = item.ts
-  const update = (await updatesTable.read({
-    maxRecords: 1,
-    filterByFormula: `{Message Timestamp} = '${ts}'`
-  }))[0]
-  if (!update) {
-    console.log('reaction was added to a message in a thread. skipping...')
-    return
-  }
+    let postExists
+    let reactionExists
+    await Promise.all([
+      (async () => {
+        postExists = await updateExists(update.fields['ID'])
+      }),
+      (async () => {
+        reactionExists = await emojiExists(reaction, update.fields['ID'])
+      })
+    ])
 
-  let postExists
-  let reactionExists
-  await Promise.all([
-    (async () => {
-      postExists = await updateExists(update.fields['ID'])
-    }),
-    (async () => {
-      reactionExists = await emojiExists(reaction, update.fields['ID'])
-    })
-  ])
-
-  if (!reactionExists) {
-    // Post hasn't been reacted to yet at all, or it has been reacted to, but not with this emoji
-    console.log(`Post hasn't been reacted to at all, or it has been reacted to, but not with this emoji`)
-    await reactionsTable.create({
-      'Update': [update.id],
-      'Emoji': [emojiRecord.id],
-      'Users Reacted': [userRecord.id]
-    })
-  } else if (postExists && reactionExists) {
-    // Post has been reacted to with this emoji
-    console.log('Post has been reacted to with this emoji')
-    const reactionRecord = await getReactionRecord(reaction, update.fields['ID'])
-    let usersReacted = reactionRecord.fields['Users Reacted']
-    console.log('adding reaction')
-    await usersReacted.push(userRecord.id)
-    await reactionsTable.update(reactionRecord.id, {
-      'Users Reacted': usersReacted
-    })
-    console.log('added reaction!')
-  }
+    if (!reactionExists) {
+      // Post hasn't been reacted to yet at all, or it has been reacted to, but not with this emoji
+      console.log(`Post hasn't been reacted to at all, or it has been reacted to, but not with this emoji`)
+      await reactionsTable.create({
+        'Update': [update.id],
+        'Emoji': [emojiRecord.id],
+        'Users Reacted': [userRecord.id]
+      })
+    } else if (postExists && reactionExists) {
+      // Post has been reacted to with this emoji
+      console.log('Post has been reacted to with this emoji')
+      const reactionRecord = await getReactionRecord(reaction, update.fields['ID'])
+      let usersReacted = reactionRecord.fields['Users Reacted']
+      console.log('adding reaction')
+      await usersReacted.push(userRecord.id)
+      await reactionsTable.update(reactionRecord.id, {
+        'Users Reacted': usersReacted
+      })
+      console.log('added reaction!')
+    }
+  })
 }
--- pages/api/slack/message/reactionRemoved.js
+++ pages/api/slack/message/reactionRemoved.js
@@ -1,4 +1,9 @@
 import { unverifiedRequest, getReactionRecord, reactionsTable, updatesTable, getUserRecord } from "../../../../lib/api-utils"
+import Bottleneck from 'bottleneck'
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1
+})
 
 export default async (req, res) => {
   if (unverifiedRequest(req)) return res.status(400).send('Unverified Slack request!')
@@ -7,22 +12,24 @@ export default async (req, res) => {
   const { item, user, reaction } = req.body.event
   const ts = item.ts
 
-  const userRecord = await getUserRecord(user)
-  const update = (await updatesTable.read({
-    maxRecords: 1,
-    filterByFormula: `{Message Timestamp} = '${ts}'`
-  }))[0]
-  //console.log('reaction_removed: update record', update)
-  const reactionRecord = await getReactionRecord(reaction, update.fields['ID'])
+  limiter.schedule(() => {
+    const userRecord = await getUserRecord(user)
+    const update = (await updatesTable.read({
+      maxRecords: 1,
+      filterByFormula: `{Message Timestamp} = '${ts}'`
+    }))[0]
+    //console.log('reaction_removed: update record', update)
+    const reactionRecord = await getReactionRecord(reaction, update.fields['ID'])
 
-  let usersReacted = reactionRecord.fields['Users Reacted']
-  const updatedUsersReacted = usersReacted.filter(userReacted => userReacted != userRecord.id)
+    let usersReacted = reactionRecord.fields['Users Reacted']
+    const updatedUsersReacted = usersReacted.filter(userReacted => userReacted != userRecord.id)
 
-  if (updatedUsersReacted.length === 0) {
-    await reactionsTable.delete(reactionRecord.id)
-  } else {
-    await reactionsTable.update(reactionRecord.id, {
-      'Users Reacted': updatedUsersReacted
-    })
-  }
+    if (updatedUsersReacted.length === 0) {
+      await reactionsTable.delete(reactionRecord.id)
+    } else {
+      await reactionsTable.update(reactionRecord.id, {
+        'Users Reacted': updatedUsersReacted
+      })
+    }
+  })
 }
