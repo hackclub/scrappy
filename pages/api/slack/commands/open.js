a53ff1b5fa721d9cce5bb3f1c25ab7cdcb9d514f
Matthew Stanciu <mattbstanciu@gmail.com>
2020-07-14 01:51:55 -0400

    ok i think fix

--- pages/api/slack/commands/open.js
+++ pages/api/slack/commands/open.js
@@ -6,9 +6,9 @@ export default async (req, res) => {
 
   const { text, user_id, response_url } = req.body
   console.log('Scrapbook open:', text)
-  const args = text.split(' ')
-  const userArg = text.split(' ')[args[0] === 'open' ? 1 : 0]?.split('@')[1].split('|')[0]
-  if (userArg && !user.includes('<@')) {
+  const args = text?.split(' ')
+  const userArg = args[args[0] === 'open' ? 1 : 0]?.split('@')[1].split('|')[0]
+  if (userArg && !userArg.includes('<@')) {
     return sendCommandResponse(response_url, t('messages.open.invaliduser'))
   }
   console.log('user arg', userArg)
