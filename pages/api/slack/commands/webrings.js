2594e88e998ff8c877b94e465a27a92cc0de9137
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-07-24 00:25:01 +0800

    Really clean code

--- pages/api/slack/commands/webrings.js
+++ pages/api/slack/commands/webrings.js
@@ -36,23 +36,21 @@ export default async (req, res) => {
   console.log('current webrings', currentWebring)
   if (!currentWebring) {
     currentWebring = [webringUserRecord.id]
+  } else if (currentWebring.length >= 8) {
+    sendCommandResponse(response_url, t(`messages.webring.toolong`))
+  } else if (!currentWebring.includes(webringUserRecord.id)) {
+    currentWebring.push(webringUserRecord.id)
+    sendCommandResponse(
+      response_url,
+      t(`messages.webring.add`, { webringUser, scrapbookLink })
+    )
   } else {
-    if (!currentWebring.includes(webringUserRecord.id)) {
-      currentWebring.push(webringUserRecord.id)
-      sendCommandResponse(
-        response_url,
-        t(`messages.webring.add`, { webringUser, scrapbookLink })
-      )
-    } else {
-      currentWebring = currentWebring.filter(
-        (rec) => rec != webringUserRecord.id
-      )
-      sendCommandResponse(
-        response_url,
-        t(`messages.webring.remove`, { webringUser, scrapbookLink })
-      )
-    }
+    currentWebring = currentWebring.filter((rec) => rec != webringUserRecord.id)
+    sendCommandResponse(
+      response_url,
+      t(`messages.webring.remove`, { webringUser, scrapbookLink })
+    )
   }
-  await accountsTable.update(userRecord.id, { Webring: currentWebring })
-  await fetchProfile(userRecord.fields['Username'])
 }
+await accountsTable.update(userRecord.id, { Webring: currentWebring })
+await fetchProfile(userRecord.fields['Username'])
