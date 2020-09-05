ebf162b9cfc7a132bd65909677855446c1ab6c23
Matthew Stanciu <mattbstanciu@gmail.com>
2020-09-05 00:28:17 -0400

    Reply in thread when scrappy is mentioned (#102)
    * Reply in thread when scrappy is mentioned
    
    * Add scrappy as a keyword

---
+++ pages/api/slack/message/mention.js
@@ -0,0 +1,30 @@
+import { reply, t, unverifiedRequest } from "../../../../lib/api-utils"
+
+const wordList = [
+  'fuck',
+  'dumb',
+  'stupid',
+  'crap',
+  'crappy'
+]
+
+const messageContainsWord = (msg) => (
+  wordList.some(word => msg.includes(word))
+)
+
+export default async (req, res) => {
+  if (unverifiedRequest(req)) {
+    return res.status(400).send('Unverified Slack request!')
+  } else {
+    res.status(200).end()
+  }
+
+  const { channel, ts, user, text, thread_ts } = req.body.event
+
+  const containsWord = await messageContainsWord(text)
+  if (containsWord) {
+    reply(channel, thread_ts, `<@${user}> ${t('messages.mentionKeyword')}`)
+  } else {
+    reply(channel, thread_ts, t('messages.mention'))
+  }
+}
