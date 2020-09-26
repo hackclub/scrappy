4e64459d7d079773e7d7e131cd49723220e4f7cc
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-09-26 22:03:37 +0800

    Create setusername.js

---
+++ pages/api/slack/commands/setusername.js
@@ -0,0 +1,39 @@
+import {
+  unverifiedRequest,
+  getUserRecord,
+  t,
+  accountsTable,
+  sendCommandResponse,
+  rebuildScrapbookFor
+} from '../../../../lib/api-utils'
+
+export default async (req, res) => {
+  if (unverifiedRequest(req))
+    return res.status(400).send('Unverified Slack request!')
+  else res.status(200).end()
+
+  const { text, user_id, response_url } = req.body
+  const args = text.split(' ')
+  let username = args[0] === 'setusername' ? args[1] : args[0]
+  username = username?.substring(1, username.length - 1).replace(" ", "_")
+
+  const userRecord = await getUserRecord(user_id)
+  if (!username) {
+    sendCommandResponse(response_url, t('messages.username.noargs'))
+  } else {
+    // update the account with the new audio
+    await accountsTable.update(userRecord.id, {
+      'Username': username,
+    })
+
+    // force a rebuild of their site
+    await rebuildScrapbookFor(userRecord)
+
+    // hang tight while the rebuild happens before giving out the new link
+    await sendCommandResponse(
+      response_url,
+      t('messages.username.set', { url: userRecord.fields['Scrapbook URL'] })
+    )
+  }
+  res.status(200).end()
+}
