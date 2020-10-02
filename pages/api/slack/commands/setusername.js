a03f2360ba6de8441ebc5123454a35facc7fd5c4
Matthew Stanciu <mattbstanciu@gmail.com>
2020-10-02 10:41:18 -0400

    formatting opinion

--- pages/api/slack/commands/setusername.js
+++ pages/api/slack/commands/setusername.js
@@ -39,7 +39,7 @@ export default async (req, res) => {
     // hang tight while the rebuild happens before giving out the new link
     await sendCommandResponse(
       response_url,
-      t('messages.username.set', { url: 'https://scrapbook.hackclub.com/'+username })
+      t('messages.username.set', { url: `https://scrapbook.hackclub.com/${username}` })
     )
   }
   res.status(200).end()
