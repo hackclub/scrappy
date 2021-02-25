113c8705f1c7279780d14e12dc6b60afec1714bf
Matthew Stanciu <mattbstanciu@gmail.com>
2021-02-25 16:08:59 -0500

    idk

--- router.js
+++ router.js
@@ -26,10 +26,10 @@ module.exports = async (app) => {
     }
     console.log('loading file', routePath)
 
-    fs.readFile(file, (err, data) => {
-      if (err) console.log('error', error)
-      console.log('data', data.toString('utf-8'))
-    })
+    // fs.readFile(file, (err, data) => {
+    //   if (err) console.log('error', error)
+    //   console.log('data', data.toString('utf-8'))
+    // })
 
     let route = require(file)
     app.use('/api' + routePath, route)
