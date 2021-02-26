131fe67243343b0907ebd662f448cbc93f48a025
Matthew Stanciu <mattbstanciu@gmail.com>
2021-02-26 18:22:47 -0500

    fix routing issue

--- src/router.js
+++ src/router.js
@@ -31,8 +31,8 @@ module.exports = async (app) => {
     //   console.log('data', data.toString('utf-8'))
     // })
 
-    let route = require(file)
-    app.use('/api' + routePath, route)
+    //let route = require(file)
+    //app.use('/api' + routePath, route)
 
     app.all('/api' + routePath, async (req, res) => {
       console.log('hey')
