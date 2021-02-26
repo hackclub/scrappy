1269e97372d5aa5be8f5b91d83feb3ee9bfd14f0
Matthew Stanciu <mattbstanciu@gmail.com>
2021-02-26 17:06:19 -0500

    scrappy dev mention

--- src/api/slack/message/index.js
+++ src/api/slack/message/index.js
@@ -73,7 +73,7 @@ module.exports = async (req, res) => {
     event.item.channel == process.env.CHANNEL
   ) {
     method = 'reactionRemoved'
-  } else if (event.text?.includes('<@U015D6A36AG>')) {
+  } else if (event.text?.includes('<@U015D6N8X7C>')) {
     console.log('scrappy has been mentioned!')
     method = 'mention'
   } else {
