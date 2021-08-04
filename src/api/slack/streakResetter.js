9cd2c445ad0a5d74605b5e6c2bef890f627b36c9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-04 12:37:28 +0800

    dont trick scrappy

--- src/api/slack/streakResetter.js
+++ src/api/slack/streakResetter.js
@@ -12,34 +12,44 @@ import fetch from 'node-fetch'
 
 export default async (req, res) => {
   res.status(200).end()
-  const users = await accountsTable.read({
-    filterByFormula: `{Streak Count} != 0`
+  const users = await prisma.accounts.findMany({
+    where: {
+      streakCount: {
+        gt: 0
+      }
+    }
   })
   users.forEach(async (user) => {
     await timeout(500)
-    const userId = user.fields['ID']
-    const timezone = user.fields['Timezone']
-    const username = user.fields['Username']
+    const userId = user.slackID
+    const timezone = user.timezone
+    const username = user.username
     let now = new Date(getNow(timezone))
     now.setHours(now.getHours() - 4)
 
-    const latestUpdate = await updatesTable.read({
-      maxRecords: 1,
-      sort: [{ field: 'Post Time', direction: 'desc' }],
-      filterByFormula: `{Poster ID} = '${userId}'`
+    const latestUpdate = await prisma.updates.findFirst({
+      where: {
+        accountsSlackID: userId
+      },
+      orderBy: [
+        {
+          postTime: 'desc'
+        }
+      ]
     })
-    const createdTime = latestUpdate[0]?.fields['Post Time']
+    const createdTime = latestUpdate[0]?.postTime
     const createdDate = new Date(createdTime).getDate()
     console.log(username, now.getDate(), new Date(createdTime).getDate())
 
-    if (shouldReset(now, createdDate) && user.fields['Streak Count'] != 0) {
+    if (shouldReset(now, createdDate) && user.streakCount != 0) {
       console.log(
         `It's been more than a day since ${username} last posted. Resetting their streak...`
       )
-      accountsTable.update(user.id, {
-        'Streak Count': 0
+      await prisma.accounts.update({
+        where: { slackID: user.slackID },
+        data: { streakCount: 0 }
       })
-      if (user.fields['Display Streak']) {
+      if (user.displayStreak) {
         await setStatus(userId, '', '')
         await fetch('https://slack.com/api/chat.postMessage', {
           method: 'POST',
@@ -60,9 +70,15 @@ export default async (req, res) => {
 const shouldReset = (now, createdDate) => {
   if (createdDate === 30 && (now.getDate() === 1 || now.getDate() === 2)) {
     return now.getDate() - createdDate > -29
-  } else if (createdDate === 31 && (now.getDate() === 1 || now.getDate() === 2)) {
+  } else if (
+    createdDate === 31 &&
+    (now.getDate() === 1 || now.getDate() === 2)
+  ) {
     return now.getDate() - createdDate > -30
-  } else if (createdDate === 1 && (now.getDate() === 30 || now.getDate() === 31)) {
+  } else if (
+    createdDate === 1 &&
+    (now.getDate() === 30 || now.getDate() === 31)
+  ) {
     return false
   } else {
     return now.getDate() - createdDate > 1
