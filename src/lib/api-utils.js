1a64a416a911defcc14160e57d918b3ff8fef664
Ella <72365100+eilla1@users.noreply.github.com>
2021-09-06 17:26:37 -0700

    Change to :aom-21: (autumn) emoji (#233)

--- src/lib/api-utils.js
+++ src/lib/api-utils.js
@@ -799,7 +799,7 @@ export const incrementStreakCount = (userId, channel, message, ts) =>
     )
     // remove beachball react
     await react('remove', channel, ts, 'beachball')
-    await react('add', channel, ts, 'summer21')
+    await react('add', channel, ts, 'aom-21')
 
     try {
       if(userRecord.webhookURL){
