b1bd1d74a420dddc30c3d59789843bf9424bf799
Arcade Wise <l3gacy.b3ta@gmail.com>
2021-09-19 21:55:43 +0000

    feat: add new curse detection

--- src/api/slack/message/mention.js
+++ src/api/slack/message/mention.js
@@ -1,18 +1,24 @@
 import { reply, t, unverifiedRequest } from '../../../lib/api-utils'
 
 const wordList = [
-  'fuck',
   'dumb',
   'suck',
   'stupid',
-  'crap',
-  'crappy',
   'trash',
   'trashy'
 ]
 
-const messageContainsWord = (msg) => (
-  wordList.some(word => msg.includes(word))
+const cursesList = [
+  'fuck',
+  'bitch',
+  'shit',
+  'fudge',
+  'crap',
+  'crappy'
+]
+
+const messageContainsWord = (list, msg) => (
+  list.some(word => msg.includes(word))
 )
 export default async (req, res) => {
   if (unverifiedRequest(req)) {
@@ -23,8 +29,11 @@ export default async (req, res) => {
 
   const { channel, ts, user, text, thread_ts } = req.body.event
 
-  const containsWord = await messageContainsWord(text)
-  if (containsWord) {
+  const containsWord = await messageContainsWord(wordList, text)
+  const containsCurse = await messageContainsWord(cursesList, text)
+  if (containsCurse) {  
+    reply(channel, thread_ts || ts, t('messages.mentionCurse', {user}))
+  } else if (containsWord) {
     reply(channel, thread_ts || ts, t('messages.mentionKeyword', {user}))
   } else {
     reply(channel, thread_ts || ts, t('messages.mention', {user}))
