bd5d13fd5c7f83d0a8d66943ebfe0055050328d9
Maggie Liu <63619830+maggie-j-liu@users.noreply.github.com>
2022-01-26 15:39:22 -0800

    delegate subdomains owned by other vercel accounts (#295)

--- src/api/slack/commands/setdomain.js
+++ src/api/slack/commands/setdomain.js
@@ -19,14 +19,6 @@ export default async (req, res) => {
   if (!arg) {
     sendCommandResponse(command.response_url, t('messages.domain.noargs'))
   } else {
-    const updates = await prisma.accounts.findMany({
-      where: {
-        customDomain: {
-          contains: '.'
-        }
-      }
-    })
-
     const user = await getUserRecord(command.user_id)
     if (user.customDomain != null) {
       console.log('DOMAIN ALREADY SET')
@@ -60,10 +52,48 @@ export default async (req, res) => {
         console.log(`Error while setting custom domain ${arg}: ${err}`)
       })
     console.log(vercelFetch)
-    if (vercelFetch.error) {
+    // domain is owned by another Vercel account, but we can request a delegation
+    if (vercelFetch.error?.code === 'forbidden') {
+      const delegationReq = await fetch(
+        `https://api.vercel.com/v6/domains/${arg}/request-delegation`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.VC_SCRAPBOOK_TOKEN}`
+          }
+        }
+      )
+        .then((r) => r.json())
+        .catch((err) => {
+          console.log(`Error while delegating custom domain ${arg}: ${err}`)
+        })
+
+      if (delegationReq.error) {
+        // something went wrong, such as the domain being an apex domain
+        sendCommandResponse(
+          command.response_url,
+          t('messages.domain.domainerror', {
+            text: arg,
+            error: delegationReq.error.message
+          })
+        )
+      } else {
+        // successfully requested delegation
+        sendCommandResponse(
+          command.response_url,
+          t('messages.domain.domaindelegated', {
+            text: arg
+          })
+        )
+      }
+    } else if (vercelFetch.error) {
       sendCommandResponse(
         command.response_url,
-        t('messages.domain.domainexists', { text: arg })
+        t('messages.domain.domainerror', {
+          text: arg,
+          error: vercelFetch.error.message
+        })
       )
     } else {
       await prisma.accounts.update({
