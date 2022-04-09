f41bcddd8017987a1b60ecc4b40d2087814253aa
Ella <git@ella.cx>
2022-04-08 18:21:41 -0700

    Add some emoji keywords (#332)
    * Spring of making
    
    * Awef
    
    * Add more keywords
    
    * Robot
    
    * :thug_lachlan: is not an emoji
    
    * Redwood

--- src/lib/channelKeywords.js
+++ src/lib/channelKeywords.js
@@ -10,7 +10,15 @@ export default {
   "C0166QHR0HG": "swift",
   "C012LPZUAPR": "gopher",
   "C14D3AQTT": "package",
-  "CD543U2UD": "thug_lachlan",
+  "CD543U2UD": "lachlan", // #lachlanfans
   "CDDMDRJUA": "hacktoberfest",
-  "C019RMWTECD": "ussr"
+  "C019RMWTECD": "ussr",
+  "CDLBHGUQN": "cat", // #cats
+  "CDJV1CXC2": "dog", // #dogs
+  "C01504DCLVD": "scrappy", // #scrapbook
+  "C01NQTDFUR5": "scrappy", // #scrappy-dev
+  "C02TWKX227J": "wordle",
+  "C0P5NE354": "robot_face", // #bot-spam
+  "C01GF9987SL": "aoc", // #adventofcode
+  "C02UN35M7LG": "gamelab", // #gamelab-dev
 }
