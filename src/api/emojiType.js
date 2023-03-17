60437f06012c616bb5dc19984bccb3347007d7e3
Sam Poder <sam.r.poder@gmail.com>
2023-03-17 20:49:50 +0800

    Create

---
+++ src/api/emojiType.js
@@ -0,0 +1,6 @@
+import prisma from '../lib/prisma.js'
+// Only runs when a user uploads a large video, to notify them when Mux processes the video
+export default async (req, res) => {
+  let updates = await prisma.emojiType.findMany()
+  res.status(200).json(updates)
+}
