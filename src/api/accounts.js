1905967361a7835a01c9ba440369ed76b74231b4
Sam Poder <39828164+sampoder@users.noreply.github.com>
2023-03-18 09:40:48 +0800

    Revert "Create"
    This reverts commit fa82532dc0a2cd7ad682bf0dfbb4bae5af85ed6d.

--- src/api/accounts.js
+++
@@ -1,6 +0,0 @@
-import prisma from '../lib/prisma.js'
-// Only runs when a user uploads a large video, to notify them when Mux processes the video
-export default async (req, res) => {
-  let updates = await prisma.accounts.findMany()
-  res.status(200).json(updates)
-}
