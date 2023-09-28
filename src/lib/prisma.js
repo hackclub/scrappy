a00845d86c2c6d6627bf23fc6ef2a83a16497310
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-28 16:10:28 +0100

    measure prisma operation latency

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,24 @@
 import Prisma from "@prisma/client";
+import metrics from "../metrics.js";
 
-let prisma = new Prisma.PrismaClient();
+let prisma = new Prisma.PrismaClient().$extends({
+  query: {
+    async $allOperations({ operation, model, args, query }) {
+      const metricKey = `${operation}_${model}`;
+      try {
+        const start = new Date().getTime();
+        const queryResult = await query(args);
+        const time = new Date().getTime() - start;
+
+        metrics.timing(metricKey, time);
+
+        return queryResult;
+      } catch (err) {
+        metrics.increment(`errors.${metricKey}`, 1);
+      }
+      return;
+    }
+  }
+});
 
 export default prisma;
