f2c23f6ac3d66bfac6f506058163ba92509adebb
JosiasAurel <ndjosiasaurel@gmail.com>
2023-12-22 12:33:31 +0100

    add fetch utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
 import emoji from "node-emoji";
 import { getUserRecord } from "./users.js";
 import { app } from "../app.js";
+import { v4 as uuidv4 } from "uuid";
 
 const replaceEmoji = (str) => emoji.emojify(str.replace(/::(.*):/, ":"));
 
@@ -63,3 +64,47 @@ export const getUrlFromString = (str) => {
   if (url.startsWith("<")) url = url.substring(1, url.length - 1);
   return url;
 };
+
+// returns the urls that are in the text
+export function getUrls(text) {
+  const matcher = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()'@:%_\+.~#?!&//=]*)/gi;
+  return text.match(matcher);
+}
+
+export function extractOgUrl(htmlDoc) {
+  const result = RegExp("og:image").exec(htmlDoc);
+
+  if (!result) return;
+
+  let index = result.index;
+  for(;;) {
+    if (htmlDoc[index] === "/" && htmlDoc[index+1] === ">") break;
+    if (htmlDoc[index] === ">") break;
+    index++;
+  }
+
+  const ogExtract = htmlDoc.slice(result.index, index);
+  const ogUrlString = ogExtract.split("content=")[1].trim();
+  return ogUrlString.slice(1, -1);
+}
+
+export async function getPageContent(page) {
+  const response = await fetch(page);
+  const content = await response.text();
+  return content;
+}
+
+export async function getAndUploadOgImage(url) {
+  const file = await fetch(url);
+  let blob = await file.blob();
+  const form = new FormData();
+  form.append("file", blob, `${uuidv4()}.png`);
+
+  const response = await fetch("https://bucky.hackclub.com", {
+    method: "POST",
+    body: form
+  });
+
+  const responseContent = await response.text();
+  return responseContent;
+}
